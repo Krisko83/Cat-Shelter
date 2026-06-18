151beccd6d8eb8389ca3e5ceee1270ff14baf421
Krisko83 <kriss_eidg@yahoo.com>
2026-06-18 19:39:03 +0300

    Launch

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ const server = http.createServer((req, res) => {
 
 
  
-server.listen(5001, () => console.log('Server is running on http://localhost:5000/'));
+server.listen(5000, () => console.log('Server is running on http://localhost:5000/'));
