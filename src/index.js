7427e01f775c7551d76666c755a9c0313ca3f5a0
Krisko83 <kriss_eidg@yahoo.com>
2026-06-18 20:08:23 +0300

    Working styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import http from 'http';
 import fs from 'fs/promises';
- 
+
 
 const server = http.createServer(async (req, res) => {
-    const homePage = await fs.readFile('./views/home/index.html', 'utf-8');
-    
-    if(req.url === '')
+console.log(req.url);
 
-    
-    res.writeHead(200, { 'content-type': 'text/html'})
+    if (req.url === '/content/styles/site.css') {
+
+        const style = await fs.readFile('./content/styles/site.css', 'utf-8');
+        res.writeHead(200, { 'Content-type': 'text/css' })
+        res.write(style);
+        return res.end();
+    }
+
+    const homePage = await fs.readFile('./views/home/index.html', 'utf-8');
+
+    res.writeHead(200, { 'content-type': 'text/html' })
     res.write(homePage)
- 
+
     res.end();
 })
 
 
- 
+
 server.listen(5000, () => console.log('Server is running on http://localhost:5000...'));
