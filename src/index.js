45d7a5e8c24a2450adfffd83357ff420fb594a12
Krisko83 <kriss_eidg@yahoo.com>
2026-06-18 19:55:13 +0300

    Loading HTML

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import http from 'http';
 import fs from 'fs/promises';
+ 
 
-const server = http.createServer((req, res) => {
-    // const homePage = fs.readFile('./views/home/index.html')
-    res.write('hello')
+const server = http.createServer(async (req, res) => {
+    const homePage = await fs.readFile('./views/home/index.html', 'utf-8');
+    
+    if(req.url === '')
+
+    
+    res.writeHead(200, { 'content-type': 'text/html'})
+    res.write(homePage)
  
     res.end();
 })
 
 
  
-server.listen(5000, () => console.log('Server is running on http://localhost:5000/'));
+server.listen(5000, () => console.log('Server is running on http://localhost:5000...'));
